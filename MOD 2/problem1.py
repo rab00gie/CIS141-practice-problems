461ca9a92bd7d1965af71d40d81f35e6170d8a80
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-04-18 07:19:18 -0700

    Delete MOD 2 directory

--- MOD 2/problem1.py
+++
@@ -1,5 +0,0 @@
-'''# 1. Create a program that prints the following output to the screen: 
-"Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. 
-Then, everything changed when the Fire Nation attacked."'''
-
-print("Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then everything changed when the Fire Nation attacked.")
