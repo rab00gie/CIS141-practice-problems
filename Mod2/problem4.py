daa697a587328eb8d2ed868a169756c8d1c0b1a9
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-04-18 15:22:44 -0700

    Update problem4.py

--- Mod2/problem4.py
+++ Mod2/problem4.py
@@ -1,3 +1,6 @@
+'''# 4. Create a program that prompts the user for their birth year and displays a message that says "You are ___ old". 
+Use an f-string in your solution to this problem.'''
+
 # Get the birth year 
 birth_year = int(input("Enter your birth year: "))
 
