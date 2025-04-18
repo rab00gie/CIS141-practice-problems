2afea3d9dcb6a50acba9d8a1b1efe9a1fc18a8b8
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-04-18 14:40:30 -0700

    Update problem2.py

--- Mod2/problem2.py
+++ Mod2/problem2.py
@@ -1,1 +1,8 @@
 
+'''# 2. Create a program that prompts for 2 numbers and then outputs
+the addition, subtraction, multiplication, and division of the first number by the second number.'''
+
+print(4+6)
+print(4-6)
+print(4*6)
+print(4/6)
