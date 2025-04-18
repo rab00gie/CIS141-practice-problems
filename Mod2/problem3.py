d05b14c5504bda5f02154a98622bb7bddab02d7e
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-04-18 15:08:22 -0700

    Update problem3.py

--- Mod2/problem3.py
+++ Mod2/problem3.py
@@ -1,1 +1,14 @@
+# Get the side lengths from the user
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+
+
+s = (a + b + c) / 2
+
+# Calculate area using Heron's formula
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+# Show the result
+print("The area of the triangle is:", area)
 
