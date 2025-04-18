dbaa08c1136451694539af9c235a794c0c03f14b
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-04-18 15:08:52 -0700

    Update problem4.py

--- Mod2/problem4.py
+++ Mod2/problem4.py
@@ -1,1 +1,11 @@
+# Get the birth year 
+birth_year = int(input("Enter your birth year: "))
 
+# Set the current year
+current_year = 2025
+
+# Calculate age
+age = current_year - birth_year
+
+# Display the message 
+print(f"You are {age} years old.")
