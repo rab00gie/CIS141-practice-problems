3052118973c26d9e6f386a51655717ea25224661
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-05-01 21:51:43 -0700

    Create Mod4_practice_prob1.py

---
+++ Mod4/Mod4_practice_prob1.py
@@ -0,0 +1,8 @@
+# Truth Table for (A AND B) OR (NOT B)
+#
+# A      B      A AND B    NOT B     (A AND B) OR (NOT B)
+# --------------------------------------------------------
+# False  False   False      True             True
+# False  True    False      False            False
+# True   False   False      True             True
+# True   True    True       False            True
