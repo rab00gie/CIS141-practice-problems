3944ed7b9e398f9ee0f9520d142d7054a5bb3396
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-05-01 21:53:25 -0700

    Create Mod4_practice_prob3.py

---
+++ Mod4/Mod4_practice_prob3.py
@@ -0,0 +1,9 @@
+'''#3. Prompt the user for their bank balance. 
+Evaluate whether the balance is less than $100. Print True
+if the user’s balance is below $100; print False, otherwise.'''
+
+# Ask user for the bank account balance.
+balance = float(input("Enter your bank balance:"))
+
+#Check if thr balance is less than $100
+print(balance < 100)
