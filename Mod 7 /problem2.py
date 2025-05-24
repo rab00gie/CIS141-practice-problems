d5e531781cf84a82a60de81c87fcbbfbd6d8dd9f
Ryan Harris <138336595+rab00gie@users.noreply.github.com>
2025-05-23 20:08:15 -0700

    problem2.py

---
+++ Mod 7 /problem2.py
@@ -0,0 +1,16 @@
+'''
+#2. Write a function called is_palindrome(s) that checks whether a string is a palindrome
+(reads the same forward and backward, ignoring case). The function should
+returns either True or False.
+'''
+
+
+
+
+def is_palindrome(s):
+    s = s.lower()
+    return s == s[::-1]
+
+print(is_palindrome("Racecar"))
+print(is_palindrome("Radar"))
+print(is_palindrome("castle"))
